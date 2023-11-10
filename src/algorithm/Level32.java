04a1c803941dac75153a3760897857fea06e8693
eqrw105 <eqrw105@naver.com>
2023-11-10 21:59:47 +0900

    매출액의 종류 타임아웃 수정

--- src/algorithm/Level32.java
+++ src/algorithm/Level32.java
@@ -1,5 +1,6 @@
 package algorithm;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 /**
@@ -16,11 +17,13 @@ public class Level32 {
 		for (int i = 0 ; i < n; i++) {
 			arr[i] = in.nextInt();
 		}
-		System.out.print(solution(n, k, arr));
+		for(Integer x : solution(n, k, arr)) {
+			System.out.print(x + " ");	
+		}
 	}
 	
-	private static String solution(int n, int k, int[] arr) {
-		String answer = "";
+	private static ArrayList<Integer> solution(int n, int k, int[] arr) {
+		ArrayList<Integer> answer = new ArrayList<>();
 		
 		HashMap<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < k - 1; i++) {
@@ -29,7 +32,7 @@ public class Level32 {
 		int lt = 0;
 		for (int rt = k - 1; rt < n; rt++) {
 			map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
-			answer += map.size() + " ";
+			answer.add(map.size());
 			map.put(arr[lt], map.get(arr[lt]) - 1);
 			if (map.get(arr[lt]) == 0) map.remove(arr[lt]);
 			lt++;
