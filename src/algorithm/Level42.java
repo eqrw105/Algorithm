e4ead43df1749fdddc2a81b0078b723e5b923ec4
eqrw105 <eqrw105@naver.com>
2023-11-21 21:41:25 +0900

    응급실 클래스 생성 방식

--- src/algorithm/Level42.java
+++ src/algorithm/Level42.java
@@ -6,28 +6,36 @@ import java.util.*;
  * 대기목록에서 꺼낸 환자보다 위험도가 높은 환자가 존재하면 대기목록 제일 뒤로 넣는다.
  * M번째 환자는 몇 번째로 진료 받는지 출력
  */
+class Person {
+	int id;
+	int priority;
+	public Person(int id, int priority) {
+		this.id = id;
+		this.priority = priority;
+	}
+}
 public class Level42 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int m = in.nextInt();
-		Queue<Map.Entry<Integer,Integer>> queue = new LinkedList<>();
+		Queue<Person> queue = new LinkedList<>();
 		for (int i = 0; i < n; i++) {
-			queue.offer(new AbstractMap.SimpleEntry<>(i, in.nextInt()));
+			queue.offer(new Person(i, in.nextInt()));
 		}
 		System.out.print(solution(n, m, queue));
 	}
 	
-	private static int solution(int n, int m, Queue<Map.Entry<Integer, Integer>> queue) {
+	private static int solution(int n, int m, Queue<Person> queue) {
 		int answer = 0;
 		while(!queue.isEmpty()) {
-			Map.Entry<Integer, Integer> entry = queue.poll();
-			if (isOver(entry.getValue(), queue)) {
-				queue.offer(entry);
+			Person person = queue.poll();
+			if (isOver(person.priority, queue)) {
+				queue.offer(person);
 			} else {
 				answer++;
-				if (entry.getKey() == m) {
+				if (person.id == m) {
 					break;
 				}
 			}
@@ -35,10 +43,10 @@ public class Level42 {
 		return answer;
 	}
 	
-	private static boolean isOver(int num, Queue<Map.Entry<Integer, Integer>> queue) {
-		Iterator<Map.Entry<Integer, Integer>> iterator = queue.iterator();
+	private static boolean isOver(int num, Queue<Person> queue) {
+		Iterator<Person> iterator = queue.iterator();
 		while(iterator.hasNext()) {
-			if (num  < iterator.next().getValue()) {
+			if (num  < iterator.next().priority) {
 				return true;
 			}
 		}
